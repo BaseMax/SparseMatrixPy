74ff84f1c380d5ea188604e32737e3cb66bda667
Max Base <maxbasecode@gmail.com>
2022-11-01 16:37:42 +0100

    convert space4 to tab

--- SparseMatrix.py
+++ SparseMatrix.py
@@ -1,37 +1,39 @@
 class SparseMatrix:
-    def __init__(self, rows, cols):
-        self.rows = rows
-        self.cols = cols
-        self.data = {}
+	def __init__(self, rows, cols):
+		self.rows = rows
+		self.cols = cols
+		self.data = {}
 
-    def __getitem__(self, index):
-        return self.data.get(index, 0)
+	def __getitem__(self, index):
+		return self.data.get(index, 0)
 
-    def __setitem__(self, index, value):
-        self.data[index] = value
+	def __setitem__(self, index, value):
+		self.data[index] = value
 
-    def __add__(self, other):
-        assert self.rows == other.rows and self.cols == other.cols
-        result = SparseMatrix(self.rows, self.cols)
-        for index in self.data:
-            result[index] = self[index]
-        for index in other.data:
-            result[index] += other[index]
-        return result
+	def __add__(self, other):
+		assert self.rows == other.rows and self.cols == other.cols
 
-    def __mul__(self, other):
-        assert self.cols == other.rows
-        result = SparseMatrix(self.rows, other.cols)
-        for i in range(self.rows):
-            for j in range(other.cols):
-                for k in range(self.cols):
-                    result[i, j] += self[i, k] * other[k, j]
-        return result
+		result = SparseMatrix(self.rows, self.cols)
+		for index in self.data:
+			result[index] = self[index]
+		for index in other.data:
+			result[index] += other[index]
+		return result
 
-    def __str__(self):
-        result = []
-        for i in range(self.rows):
-            for j in range(self.cols):
-                result.append(str(self[i, j]).rjust(8))
-            result.append("\n")
-        return ''.join(result)
+	def __mul__(self, other):
+		assert self.cols == other.rows
+
+		result = SparseMatrix(self.rows, other.cols)
+		for i in range(self.rows):
+			for j in range(other.cols):
+				for k in range(self.cols):
+					result[i, j] += self[i, k] * other[k, j]
+		return result
+
+	def __str__(self):
+		result = []
+		for i in range(self.rows):
+			for j in range(self.cols):
+				result.append(str(self[i, j]).rjust(8))
+			result.append("\n")
+		return ''.join(result)
